702233edb2139bf4eb2e5f38aab9cdedaf5eaffa
euhjainy <157803472+euhjainy@users.noreply.github.com>
2025-01-20 22:12:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+
+
+$(document).ready(function () {
+  // Initialisation du carrousel
+  $('.your-class').slick({
+    infinite: true,
+    adaptiveHeight: true,
+  
+  });
+
+ 
+  
+
+  const leftSection = document.querySelector('.section-left');
+  const rightSection = document.querySelector('.section-right');
+  const centerSection = document.querySelector('.section-center');
+
+  // Fonction pour agrandir la section gauche
+  function expandLeft() {
+    leftSection.style.flex = '0.8'; // La section gauche prend 80% de l'espace
+    centerSection.style.flex = '0.2'; // La section centrale est réduite
+    rightSection.style.flex = '0'; // La section droite est réduite
+
+    // Attendre la fin de la transition CSS et réinitialiser le carrousel
+    setTimeout(() => {
+      $('.your-class').slick('setPosition');
+    }, 200); // La durée correspond à la transition CSS (0.5s)
+  }
+
+  // Fonction pour agrandir la section droite
+  function expandRight() {
+    leftSection.style.flex = '0'; // La section gauche est réduite
+    centerSection.style.flex = '0.2'; // La section centrale est réduite
+    rightSection.style.flex = '0.8'; // La section droite prend 80% de l'espace
+
+    // Attendre la fin de la transition CSS et réinitialiser le carrousel
+    setTimeout(() => {
+      $('.your-class').slick('setPosition');
+    }, 200);
+  }
+
+  // Fonction pour réinitialiser les sections
+  function resetSections() {
+    leftSection.style.flex = '0.4'; // Taille initiale 40%
+    centerSection.style.flex = '0.2'; // Taille initiale 20%
+    rightSection.style.flex = '0.4'; // Taille initiale 40%
+
+    // Attendre la fin de la transition CSS et réinitialiser le carrousel
+    setTimeout(() => {
+      $('.your-class').slick('setPosition');
+    }, 200);
+  }
+
+  // Associer les fonctions aux boutons
+  window.expandLeft = expandLeft;
+  window.expandRight = expandRight;
+  window.resetSections = resetSections;
+});
+
+
+
+
+
+
+
+
+var cursor = document.getElementById("cursor");
+document.body.addEventListener("mousemove", function(e) {
+  cursor.style.left = e.clientX + "px",
+    cursor.style.top = e.clientY + "px";
+});
+
+
+
